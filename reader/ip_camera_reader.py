2339b9ad1603f1025066db965c8ec14998f0f770
Sämir Hamadi <samir.hamadi@gofore.com>
2018-09-16 22:51:53 +0300

    RTSP local nw streaming for ismartviewpro

---
+++ reader/ip_camera_reader.py
@@ -0,0 +1,30 @@
+from detect import detect_objects as detect
+from render import draw
+import cv2
+
+frame_limit = 0
+
+cap = cv2.VideoCapture('rtsp://192.168.1.121/live/ch0')
+
+frame_nbr = 0
+
+def start():
+    while True:
+        global frame_nbr
+        frame_nbr = frame_nbr + 1
+
+        grabbed, frame = cap.read()
+
+        if grabbed:
+            detect.detectCircles(frame)
+            draw.draw(frame)
+            cv2.imshow("frame", frame)
+
+            frame_nbr = 0
+
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
